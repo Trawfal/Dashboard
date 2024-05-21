857c0e49112647409e22cc63c593297b8a20a975
Eduardo Kevin <kevinbdw2@gmail.com>
2024-05-21 20:04:51 -0300

    second commit

--- tentativa1.py
+++ tentativa1.py
@@ -16,6 +16,8 @@ st.set_page_config(layout="wide")
 df = pd.read_csv("Fires.csv", header=0, encoding='latin-1')
 df["Start_Date"] = pd.to_datetime(df["Start_Date"])
 df["End_Date"] = pd.to_datetime(df["End_Date"])
+df["Longitude"].astype(str).astype(float)
+df["Latitude"].astype(str).astype(float)
 
 df["Month"] = df["Start_Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
 month = st.sidebar.selectbox("Meses dos anos", df["Month"].unique())
@@ -26,9 +28,15 @@ df_filtered
 col1, col2 = st.columns(2)
 col3, col4, col5 = st.columns(3)
 
-fig_date = px.bar(df_filtered, x="Start_Date", y="Total_hectares", title="")
+fig_date = px.bar(df_filtered, x="Start_Date", y="Total_hectares", color="Total_hectares", title="Total of hectares burned")
 col1.plotly_chart(fig_date)
+fig_date2 = px.bar(df_filtered, x="Start_Date", y="Cause", color="Cause", title="Cause of burned hectares")
+col2.plotly_chart(fig_date2)
 
-
-
-
+fig = px.density_mapbox(df, lat = 'Latitude', lon = 'Longitude', z = 'Total_hectares',
+                        radius = 15,
+                        center = dict(lat = 42.83, lon = -8.35),
+                        zoom = 3,
+                        color_continuous_scale=px.colors.cyclical.IceFire,
+                        mapbox_style = 'carto-positron')
+fig.show()
