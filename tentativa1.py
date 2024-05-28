b2e86bd73b2c6a523652ca158d0cf406e2ed1580
Eduardo Kevin <kevinbdw2@gmail.com>
2024-05-28 19:26:12 -0300

    fourth commit

--- tentativa1.py
+++ tentativa1.py
@@ -4,7 +4,8 @@ import plotly.express as px
 import numpy as np
 
 st.set_page_config(layout="wide")
-st.title("Dashboard sobre Incendios florestais no Mexico")
+st.title(":orange[Incendios florestais no Mexico ]")
+st.subheader("Ainda pensando no que escrever")
 
 #Media de hectares queimado por ano
 #Cidades com mais recorrencias de incendios
@@ -15,42 +16,78 @@ st.title("Dashboard sobre Incendios florestais no Mexico")
 #Horario de comeco relacionado com a causa
 
 df = pd.read_csv("Fires.csv", header=0, encoding='latin-1')
+
 df["Start_Date"] = pd.to_datetime(df["Start_Date"])
 df["End_Date"] = pd.to_datetime(df["End_Date"])
 df["Longitude"].astype(str).astype(float)
 df["Latitude"].astype(str).astype(float)
 
-df["Month"] = df["Start_Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
-month = st.sidebar.selectbox("Meses dos anos", df["Month"].unique())
+df["Year"] = df["Start_Date"].apply(lambda x: str(x.year))
+df["Month"] = df["Start_Date"].apply(lambda x: str(x.month))
 
-df_filtered = df[df["Month"] == month]
-df_filtered
+st.sidebar.header("Please filter here:")
+year = st.sidebar.multiselect(
+    "Select the year:",
+    options=df["Year"].unique(),
+    default= "2015"
+)
 
-col1, col2 = st.columns(2)
-col3, = st.columns(1)
+month = st.sidebar.multiselect(
+    "Select the month:",
+    options=df["Month"].unique(),
+    default= "1"
+)
 
-fig_date = px.bar(df_filtered,
+df_selection = df.query(
+    "Year == @year & Month == @month"
+)
+
+st.dataframe(df_selection)
+
+
+col1, = st.columns(1)
+col2, col3 = st.columns(2)
+col4, col5 = st.columns(2)
+
+fig_date1 = px.density_mapbox(df, lat = 'Latitude', lon = 'Longitude', z = 'Total_hectares',
+                        radius = 15,
+                        center = dict(lat = 23.87, lon = -102.48),
+                        zoom = 4,
+                        height = 700,
+                        color_continuous_scale=px.colors.cyclical.IceFire,
+                        mapbox_style = 'carto-positron',
+                        title="Heatmap of Fires"
+                        )
+col1.plotly_chart(fig_date1, use_container_width=True)
+
+fig_date2 = px.bar(df_selection,
                   x="Start_Date",
                   y="Total_hectares",
                   color="Total_hectares",
                   title="Total of hectares burned"
                   )
-col1.plotly_chart(fig_date)
+col2.plotly_chart(fig_date2)
 
-fig_date2 = px.bar(df_filtered,
+fig_date3 = px.bar(df_selection,
                    x="Start_Date",
                    y="Cause",
                    color="Cause",
                    title="Cause of burned hectares"
                    )
-col2.plotly_chart(fig_date2)
+col3.plotly_chart(fig_date3)
 
-fig_date3 = px.density_mapbox(df, lat = 'Latitude', lon = 'Longitude', z = 'Total_hectares',
-                        radius = 15,
-                        center = dict(lat = 23.87, lon = -102.48),
-                        zoom = 4,
-                        color_continuous_scale=px.colors.cyclical.IceFire,
-                        mapbox_style = 'carto-positron',
-                        title="Heatmap of Fires"
-                        )
-col3.plotly_chart(fig_date3, use_container_width=True)
+fig_date4 = px.bar(df_selection,
+                  x="Start_Date",
+                  y="Total_hectares",
+                  color="Total_hectares",
+                  title="Total of hectares burned"
+                  )
+col4.plotly_chart(fig_date4)
+
+fig_date5 = px.bar(df_selection,
+                  x="Start_Date",
+                  y="Total_hectares",
+                  color="Total_hectares",
+                  title="Total of hectares burned"
+                  )
+col5.plotly_chart(fig_date5)
